0dce2fa3f87a5315e3e7ce1ced9a84610be167b5
Rhitam <banerjeerhitam1@gmail.com>
2025-06-29 20:51:13 +0530

    UPDATE:Added isStared, is Trash and delete route

---
+++ app/api/files/upload/[fileId]/trash/route.ts
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/lib/db";
+import { files } from "@/lib/db/schema";
+import { eq, and } from "drizzle-orm";
+
+export async function POST(req: NextRequest, props: { params: Promise<{ fileId: string }> }) {
+  try {
+    const { userId } = await auth()
+    if (!userId) {
+      return NextResponse.json({
+        error: "Unauthorised USER"
+      }, {
+        status: 401
+      })
+    }
+    const { fileId } = await props.params
+    if (!fileId) {
+      return NextResponse.json({
+        error: "File Id required"
+      }, {
+        status: 401
+      })
+    }
+    const [file] = await db.select().from(files).where(
+      and(
+        eq(files.id, fileId),
+        eq(files.userId, userId)
+      )
+    )
+    if (!file) {
+      return NextResponse.json({
+        error: "User File not found"
+      }, {
+        status: 401
+      })
+    }
+    const updatedFiles = await db.update(files).set({ isTrash: !files.isTrash }).where(and(
+      eq(files.id, fileId),
+      eq(files.userId, userId)
+    )).returning()
+    const updatedFile = updatedFiles[0]
+    return NextResponse.json(updatedFile)
+  } catch (error) {
+    return NextResponse.json({
+      error: "Error marking file trash"
+    }, {
+      status: 401
+    })
+  }
+}
