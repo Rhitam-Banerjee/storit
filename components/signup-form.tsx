552525b3c52777249e9512dbcedc785cd929629a
Rhitam <banerjeerhitam1@gmail.com>
2025-07-11 01:53:33 +0530

    FIX;Removed unused var

--- components/signup-form.tsx
+++ components/signup-form.tsx
@@ -13,7 +13,6 @@ import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import {
   Form,
   FormControl,
