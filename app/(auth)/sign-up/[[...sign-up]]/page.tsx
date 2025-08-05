d65e660132230c8de34784c40cf366d12b2a5768
Rhitam <banerjeerhitam1@gmail.com>
2025-08-05 13:32:02 +0530

    UPDATE:Changed component name

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,6 +2,6 @@ import { SignUpForm } from "@/components/signup-form";
 export const metadata = {
   title: "Sign-In | StorIt",
 };
-export default function LoginPage() {
+export default function SignUpPage() {
   return <SignUpForm />;
 }
